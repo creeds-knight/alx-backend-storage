ee9a4b7ac64938b4dd4ff31112f7ec9cadbb5e90
creeds-knight <apedoarthur21@gmail.com>
2024-07-18 00:00:25 +0300

    task3, adding a count decorator

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -5,6 +5,22 @@
 from typing import Union, Callable, Optional
 import redis
 import uuid
+import functools
+
+
+def count_calls(method: Callable) -> Callable:
+    """
+        Function to count keys stored in the database
+    """
+    @functools.wraps(method)
+    def wrapper(self, *args, **kwargs):
+        """
+            Wrapper function
+        """
+        key = method.__qualname__
+        self._redis.incr(key)
+        return method(self, *args, **kwargs)
+    return wrapper
 
 
 class Cache:
@@ -19,6 +35,7 @@ class Cache:
         self._redis = redis.Redis()
         self._redis.flushdb()
 
+    @count_calls
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """
             Store the data to the database

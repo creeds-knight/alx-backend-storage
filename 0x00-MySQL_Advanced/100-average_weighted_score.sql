aa9a79ae22039f7a5738d01eeca685cb6fc4597e
creeds-knight <apedoarthur21@gmail.com>
2024-09-08 17:46:24 +0300

    task 12

---
+++ 0x00-MySQL_Advanced/100-average_weighted_score.sql
@@ -0,0 +1,20 @@
+-- Task 12
+DELIMITER $$
+CREATE PROCEDURE ComputeAverageWeightedScoreForUser(user_id INT)
+BEGIN
+    DECLARE weighted_avg FLOAT;
+    SELECT DISTINCT
+        SUM(c.score * p.weight) / SUM(p.weight) INTO weighted_avg
+    FROM
+        corrections AS c
+    LEFT JOIN projects AS p
+    ON p.id = c.project_id
+    WHERE
+        c.user_id = user_id;
+
+    UPDATE users
+    SET average_score = weighted_avg
+    WHERE id = user_id;
+
+END $$
+DELIMITER ;
